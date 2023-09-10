73a421f62d556b87c3f2248e2a3be4e42a9a745f
pokharelap <144560717+pokharelap@users.noreply.github.com>
2023-09-10 13:54:12 -0500

    Project.sql

--- Project.sql
+++ Project.sql
@@ -17,7 +17,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 
-
+--- CODE to interpret data---
 
 -- Define the target year using the SET command
 SET my_vars.target_year = 2012; -- Change the year as needed
